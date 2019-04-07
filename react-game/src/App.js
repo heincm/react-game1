1d9202c73ef28bb65036168773facab27ad51eed
Chris <chris.m.hein@gmail.com>
2019-04-06 20:36:34 -0400

    images show on page

--- react-game/src/App.js
+++ react-game/src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Wrapper from './Components/Wrapper'
+import AvengerCard from './Components/AvengerCard'
+import avengers from './avengers.json'
 
 class App extends Component {
+
+  state = {
+    avengers
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        {this.state.avengers.map(avenger => (
+          <AvengerCard
+            removeFriend={this.removeFriend}
+            id={avenger.id}
+            key={avenger.id}
+            name={avenger.name}
+            image={avenger.image}
+            whatever={avenger.whatever}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
