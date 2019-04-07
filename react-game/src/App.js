0934abcdbf13dc091bc70014222d4d5bc7586b37
Chris <chris.m.hein@gmail.com>
2019-04-06 23:47:34 -0400

    updated state properties

--- react-game/src/App.js
+++ react-game/src/App.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
 import Wrapper from './Components/Wrapper'
 import AvengerCard from './Components/AvengerCard'
+import Navbar from './Components/Navbar'
 import avengers from './avengers.json'
 
 class App extends Component {
 
   state = {
-    avengers
+    avengers,
+    count: 0,
+    isClicked: false
   }
+
+  shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  handleOnClick = () => {
+    this.setState({ isClicked: true, count: this.state.count + 1 });
+    console.log(this.state.isClicked)
+    console.log(this.state.count)
+  }
+
 
   render() {
     return (
-      <Wrapper>
-        {this.state.avengers.map(avenger => (
-          <AvengerCard
-            removeFriend={this.removeFriend}
-            id={avenger.id}
-            key={avenger.id}
-            name={avenger.name}
-            image={avenger.image}
-            whatever={avenger.whatever}
-          />
-        ))}
-      </Wrapper>
+      <>
+        <Navbar count={this.state.count}/>
+        <Wrapper>
+          {this.state.avengers.map(avenger => (
+            <AvengerCard
+              shuffle={this.shuffle}
+              id={avenger.id}
+              key={avenger.id}
+              name={avenger.name}
+              image={avenger.image}
+              handleOnClick={this.handleOnClick} />
+          ))}
+        </Wrapper>
+      </>
     );
   }
 }
