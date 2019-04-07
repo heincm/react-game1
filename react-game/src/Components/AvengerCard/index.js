8479e6500066f206aa80ad0b8d0d2851554a8a53
Chris <chris.m.hein@gmail.com>
2019-04-06 20:35:26 -0400

    initial components add

---
+++ react-game/src/Components/AvengerCard/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+
+function AvengerCard(props) {
+    return (
+        <div className="card">
+            <div className="img-container">
+                <img alt={props.name} src={props.image} />
+            </div>
+            <div className="content">
+                <ul>
+                    <li>
+                        <strong>Name:</strong> {props.name}
+                    </li>
+                    <li>
+                        <strong>Occupation:</strong> {props.occupation}
+                    </li>
+                    <li>
+                        <strong>Clicked:</strong> {props.whatever}
+                    </li>
+                </ul>
+            </div>
+            <span onClick={() => props.removeFriend(props.id)} className="remove">
+                𝘅
+      </span>
+        </div>
+    );
+}
+
+export default AvengerCard;
