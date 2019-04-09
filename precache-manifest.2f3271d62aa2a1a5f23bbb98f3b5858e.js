53ba39f244ebce904e23a1780e63dfab043115dd
Chris <chris.m.hein@gmail.com>
2019-04-08 23:02:29 -0400

    Updates

---
+++ precache-manifest.2f3271d62aa2a1a5f23bbb98f3b5858e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a038c117699a7a341e8",
+    "url": "/react-game/static/js/runtime~main.ef933c85.js"
+  },
+  {
+    "revision": "ffc104d2e93b613c907d",
+    "url": "/react-game/static/js/main.7e7d5962.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/react-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "ffc104d2e93b613c907d",
+    "url": "/react-game/static/css/main.68e60c08.chunk.css"
+  },
+  {
+    "revision": "6fb8ab23c929f3913de675de40fa55f6",
+    "url": "/react-game/index.html"
+  }
+];
