ea1197b4564d64a3c807ee708a71c69f7ec4258e
Chris <chris.m.hein@gmail.com>
2019-05-26 13:57:51 -0400

    added id value

--- react-game/src/Components/Jumbotron/index.js
+++ react-game/src/Components/Jumbotron/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import "./style.css"
 
 function Jumbotron() {
     return (
-    <div className="jumbotron jumbotron-fluid bg-primary">
+    <div className="jumbotron jumbotron-fluid" id="topJumbo">
         <div className="container">
-            <h1 className="display-3">Avenger Clicky Game!</h1>
-            <p className="lead">Try not to click the same avenger twice.</p>
+            <h1 className="text-white display-3">Avenger Clicky Game!</h1>
+            <p className="text-white lead">Try not to click the same avenger twice.</p>
         </div>
     </div>
     );
